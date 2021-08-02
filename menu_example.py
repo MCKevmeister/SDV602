990554a089751e38c582a32c00ca97bfd8fc2d8a
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-08-03 11:07:46 +1200

    exercise 2: replace all menu tuple's with lists. All code functioning

--- menu_example.py
+++ menu_example.py
@@ -3,25 +3,25 @@ Menu examples
 """
 
 
-def show_menu(menu_tuple):
+def show_menu(menu_list):
     skip_first_flag = True
-    header_line = menu_tuple[0]  # 0 tuple is the header
+    header_line = menu_list[0]  # 0 tuple is the header
     print(header_line)
-    for item in menu_tuple:
+    for item in menu_list:
         if skip_first_flag:
             skip_first_flag = False
         else:
             print(f'{item[0]} {item[1]}')
 
 
-def make_valid_numbers(menu_tuple):
+def make_valid_numbers(menu_list):
     """
-    Make a set of valid numbers from a menu_tuple
+    Make a set of valid numbers from a menu_list
     """
     result = set()
     print("in make valid numbers:")
     first = True
-    for a_tuple in menu_tuple:
+    for a_tuple in menu_list:
         if first:
             first = False
         else:
@@ -29,17 +29,17 @@ def make_valid_numbers(menu_tuple):
     return result
 
 
-def run_menu(menu_tuple):
+def run_menu(menu_list):
     """
     Display a menu
     """
-    valid_numbers = make_valid_numbers(menu_tuple)  # set of valid numbers1
+    valid_numbers = make_valid_numbers(menu_list)  # set of valid numbers1
     not_done = True
     while not_done:
-        show_menu(menu_tuple)
+        show_menu(menu_list)
         the_value = get_integer_input()
         if the_value in valid_numbers:
-            the_cmd = menu_tuple[the_value][2]
+            the_cmd = menu_list[the_value][2]
             not_done = the_cmd(the_value)
         else:
             print(f"Oops! {the_value} is not a valid number")
@@ -49,7 +49,6 @@ def get_integer_input():
     """
     Input a number
     """
-    result = None
     test = None
     while True:
         try:
@@ -58,18 +57,17 @@ def get_integer_input():
             break
         except ValueError:
             print(f'Oops! {test} was not a number')
-
     return result
 
 
-def run_menu_one():
-    menu_one = ("Select a number.", (1, "Menu Burger", test_cmd), (2, "Menu Chips", test_cmd), (3, "Quit", quit_cmd))
+def run_menu_one(a_value):
+    menu_one = ["Select a number.", (1, "Menu Burger", test_cmd), (2, "Menu Chips", test_cmd), (3, "Quit", quit_cmd)]
     run_menu(menu_one)
     return True
 
 
-def run_menu_two():
-    menu_two = ("Select a number.", (1, "different", test_cmd), (2, "menu", test_cmd), (3, "items", quit_cmd))
+def run_menu_two(a_value):
+    menu_two = ["Select a number.", (1, "different", test_cmd), (2, "menu", test_cmd), (3, "items", quit_cmd)]
     run_menu(menu_two)
     return True
 
@@ -79,13 +77,13 @@ def test_cmd(a_value):
     return True  # continue
 
 
-def quit_cmd():
+def quit_cmd(a_value):
     print('Thank you, good-bye!')
     return False  # stop
 
 
 if __name__ == "__main__":
     # testing code for now
-    menu_tuple = ("Select a number.", (1, "Menu One", run_menu_one), (2, "Menu Two", run_menu_two),
-                  (3, "Menu Three", test_cmd), (4, "Quit", quit_cmd))
-    run_menu(menu_tuple)
+    menu_list = ["Select a number.", (1, "Menu One", run_menu_one), (2, "Menu Two", run_menu_two),
+                 (3, "Menu Three", test_cmd), (4, "Quit", quit_cmd)]
+    run_menu(menu_list)
