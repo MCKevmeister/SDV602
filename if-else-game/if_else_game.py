55d20c8cca8f3ae38f783595a5e17e19d46f2bfb
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-08-16 09:39:54 +1200

    defining layouts working draft

--- if-else-game/if_else_game.py
+++ if-else-game/if_else_game.py
@@ -5,12 +5,12 @@ def game():
     sg.theme('Dark Blue')
     count = 0
 
-    layout = [[sg.Text(text='Where are you?\n1.Nowhere\n2.Everywhere\n3.Somewhere', key='-QUESTION-')],
-              [sg.Button(button_text='Nowhere', key='-BUTTON1-'),
-               sg.Button(button_text='Everywhere', key='-BUTTON2-'),
-               sg.Button(button_text='Somewhere', key='-BUTTON3-')]]
+    layout = [[sg.T(text='Where are you?\n1.Nowhere\n2.Everywhere\n3.Somewhere', key='-Q-', size=(50, 1))],
+              [sg.B(key='-B1-'),
+               sg.B(key='-B2-'),
+               sg.B(button_text='Somewhere', key='-B3-')]]
 
-    window = sg.Window('Welcome to the game that never ends', layout)
+    window = sg.Window('Welcome to the game that never ends', layout, finalize=True)
 
     while True:
         event, values = window.read()
@@ -18,22 +18,23 @@ def game():
             break
         elif count == 0 and event != 'End':
             count = + 1
-            window['-QUESTION-'].update('What is the answer?\n1.No\n2.Yes\n3.Maybe')
-            window['-BUTTON1-'].update('No')
-            window['-BUTTON2-'].update('Yes')
-            window['-BUTTON3-'].update('Maybe')
+            window['-Q-']('What is the answer?\n1.No\n2.Yes\n3.Maybe')
+            window['-B1-']('No')
+            window['-B2-']('Yes')
+            window['-B3-']('Maybe')
         elif count == 1 and event != 'End':
-            count = + 1
-            window['-QUESTION-'].update('Game Over Replay?\n1.Yes\n2.No\n3.Maybe')
-            window['-BUTTON1-'].update('Yes')
-            window['-BUTTON2-'].update('No')
-            window['-BUTTON3-'].update('Maybe')
-        elif count >= 2 and event != 'End':
-            count = - 2
-            window['-QUESTION-'].update('Where are you?\n1.Nowhere\n2.Everywhere\n3.Somewhere')
-            window['-BUTTON1-'].update('Nowhere')
-            window['-BUTTON2-'].update('Everywhere')
-            window['-BUTTON3-'].update('Somewhere')
+            count = -1
+            window['-Q-']('Game Over Replay?\n1.Yes\n2.No\n3.Maybe')
+            window['-B1-']('Yes')
+            window['-B2-']('No')
+            window['-B3-']('Maybe')
+        elif count == -1 and event != 'End':
+            count = 0
+            window['-Q-']('Where are you?\n1.Nowhere\n2.Everywhere\n3.Somewhere')
+            window['-B1-']('Nowhere')
+            window['-B2-']('Everywhere')
+            window['-B3-']('Somewhere')
+
     window.close()
 
 
