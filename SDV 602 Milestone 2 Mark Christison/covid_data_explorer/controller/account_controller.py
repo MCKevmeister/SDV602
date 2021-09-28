d2504a72c295e924abbe76f7217d03ecba992c0c
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-09-28 17:20:03 +1300

    add create user method

--- SDV 602 Milestone 2 Mark Christison/covid_data_explorer/controller/account_controller.py
+++ SDV 602 Milestone 2 Mark Christison/covid_data_explorer/controller/account_controller.py
@@ -1,9 +1,18 @@
-def create_account(username, email, password):
-    # TODO
-    return 0
+import mysql.connector
 
 
-def login():
+def create_account(username, password, email):
+    args = (username, password, email)
+
+    connection = mysql.connector.connect(option_files='my.conf')
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        return cursor.callproc('RegisterUser', args)
+    connection.close()
+
+
+def login(username, password):
     # TODO
     return 0
 
