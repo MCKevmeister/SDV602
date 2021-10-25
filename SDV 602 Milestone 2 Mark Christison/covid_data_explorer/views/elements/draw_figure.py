1bfec9a1a054fa546258ff7cb2ae01b1b818b757
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-10-25 21:06:51 +1300

    refactor to draw graph

--- SDV 602 Milestone 2 Mark Christison/covid_data_explorer/views/elements/draw_figure.py
+++ SDV 602 Milestone 2 Mark Christison/covid_data_explorer/views/elements/draw_figure.py
@@ -1,12 +1,13 @@
-import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib
 
 matplotlib.use('TkAgg')
 
-fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+def get_fig(x_axis, y_axis):
+    fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+    fig.add_subplot().plot(x_axis, y_axis)
+    return fig
 
 
 def draw_figure(canvas, figure):
@@ -14,3 +15,5 @@ def draw_figure(canvas, figure):
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
+
+
