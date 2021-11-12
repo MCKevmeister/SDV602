be02c91d212dc6a59eb9a7c27ef17c8c80edd6a4
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-11-12 15:27:58 +1300

    Not used

--- SDV 602 Milestone 2 Mark Christison/covid_data_explorer/controller/account_controller.py
+++ SDV 602 Milestone 2 Mark Christison/covid_data_explorer/controller/account_controller.py
@@ -1,16 +1,15 @@
-from mysql_functions import *
-
-
-def create_account(username, password, email):
-    args = (username, password, email)
-    return call_mysql_stored_procedure('RegisterUser', args)
-
-
-def login(username, password):
-    args = (username, password)
-    return call_mysql_stored_procedure('LoginUser', args)
-
-
-def logout():
-    # TODO
-    return 0
+# from mysql_functions import *
+#
+#
+# def create_account(username, password, email):
+#     args = (username, password, email)
+#     return call_mysql_stored_procedure('RegisterUser', args)
+#
+#
+# def login(username, password):
+#     args = (username, password)
+#     return call_mysql_stored_procedure('LoginUser', args)
+#
+#
+# def logout():
+#     return 0
