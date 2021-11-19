5f831b22e401a125245c4d094633d38a59ef3e10
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-11-19 21:40:15 +1300

    add doc strings

--- SDV 602 Milestone 2 Mark Christison/covid_data_explorer/csv_data/data_plotter.py
+++ SDV 602 Milestone 2 Mark Christison/covid_data_explorer/csv_data/data_plotter.py
@@ -4,6 +4,13 @@ from get_remote_data import get_df
 
 
 def get_data_column(data_col) -> int:
+    """
+        Returns a number corresponding with the number of the row in the CSV correlated with the data
+
+        :param data_col: The data column to return
+        :type data_col: string
+        :rtype: int
+        """
     if data_col == "total_cases":
         return 4
     elif data_col == "total_vaccinations":
@@ -13,6 +20,20 @@ def get_data_column(data_col) -> int:
 
 
 def filter_data(start_date, end_date, country, data_col, df):
+    """
+        Filters a data frame within date boundaries for selected data column from a country
+
+        :param start_date: Start date to filter by
+        :type start_date: tuple[int, int, int]
+        :param end_date: End date to filter by
+        :type end_date: tuple[int, int, int]
+        :param country: Country name to filter by
+        :type country: string
+        :param data_col: Name of column to filter by
+        :type data_col: string
+        :param df: The data frame to filter from
+        :type df: dataframe
+    """
     start_datetime = pd.to_datetime(start_date, dayfirst=True)
     end_datetime = pd.to_datetime(end_date, dayfirst=True)
 
@@ -36,4 +57,4 @@ if __name__ == '__main__':
     print("got dataframe")
     print(filter_data('1/3/21', '2/3/21', 'New Zealand', 'total_cases', data_frame))
     print(filter_data('2/4/21', '20/4/21', "New Zealand", 'total_vaccinations', data_frame))
-
+    print(filter_data('2/4/21', '20/4/21', "New Zealand", 'total_deaths', data_frame))
