7559967f292019abf81a0f58e1d6dce0da51dffc
Mark Christison <53839878+MCKevmeister@users.noreply.github.com>
2021-11-19 21:40:45 +1300

    add doc strings

--- SDV 602 Milestone 2 Mark Christison/covid_data_explorer/csv_data/get_remote_data.py
+++ SDV 602 Milestone 2 Mark Christison/covid_data_explorer/csv_data/get_remote_data.py
@@ -1,18 +1,33 @@
 import pandas as pd
+from shared_data import data_source
 
 
-# Below function was previously used to create a list of countries when using a CSV file. No longer necessary
+def get_df():
+    """
+        Gets the CSV file and converts it into a pandas data frame
+    """
+    data_source.clear()
+    external_data = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv', parse_dates=[3],
+                                dayfirst=True)
+    data_source.append(external_data.values.tolist())
 
-# def get_countries():
+
+# Below function was previously used to create a list of countries when using a CSV file.
+# No longer necessary since the requirements require using remote data and country list was made
+# def get_countries_from_csv():
 #     with open('owid-covid-data.csv', newline='') as csv_file:
 #         csv_reader = csv.reader(csv_file, delimiter=',')
 #         for row in csv_reader:
-#             if row[0] not in countries:
-#                 countries.append(row[0])
+#             if row[2] not in countries:
+#                 countries.append(row[2])
 
-def get_df():
-    return pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv', parse_dates=[3], dayfirst=True)
+# def print_first_row():
+#     with open('owid-covid-data.csv', newline='') as csv_file:
+#         reader = csv.reader(csv_file)
+#         # for row in reader:
+#         #     print(" ".join(row))
+#         print(next(reader))
 
 
-if __name__ == '__main__':
-    print(get_df())
+# if __name__ == '__main__':
+#     print_first_row()
